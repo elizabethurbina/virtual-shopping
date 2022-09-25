e3c7a528f8160cbf9d17f09520f8031073c721e3
Elizabeth Silva <eliizab3th94@gmail.com>
2022-09-24 19:22:24 -0600

    fix: change publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output:{
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/virtual-shopping',
+        publicPath: 'auto',
     },
     mode: 'development',
     resolve: {
